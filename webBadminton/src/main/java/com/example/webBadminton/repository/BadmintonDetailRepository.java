0455be50d7b50bd585c01354430eee728009c374
KeloTuan <ductuan.kelo@gmail.com>
2024-06-11 20:08:50 +0700

    Nothing

--- webBadminton/src/main/java/com/example/webBadminton/repository/BadmintonDetailRepository.java
+++ webBadminton/src/main/java/com/example/webBadminton/repository/BadmintonDetailRepository.java
@@ -1,9 +1,8 @@
 package com.example.webBadminton.repository;
 
-import com.example.webBadminton.model.Badminton;
+import com.example.webBadminton.model.BadmintonDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 @Repository
-
-public interface BadmintonRepository extends JpaRepository<Badminton, Long> {
+public interface BadmintonDetailRepository extends JpaRepository<BadmintonDetail, Long> {
 }
