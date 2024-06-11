db0229a67bdb37c4686f43fd0f83cbab45c19e1d
nguyentranlht <88877923+nguyentranlht@users.noreply.github.com>
2024-06-11 07:52:17 +0700

    first commit

---
+++ webBadminton/src/main/java/com/example/webBadminton/WebBadmintonApplication.java
@@ -0,0 +1,13 @@
+package com.example.webBadminton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBadmintonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBadmintonApplication.class, args);
+	}
+
+}
---
+++ webBadminton/src/test/java/com/example/webBadminton/WebBadmintonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webBadminton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBadmintonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
