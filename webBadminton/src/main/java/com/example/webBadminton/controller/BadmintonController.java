855f6f2fb09f79d0bbc3af8f6f8d1e32a53c3137
nguyentranlht <88877923+nguyentranlht@users.noreply.github.com>
2024-06-12 09:58:05 +0700

    ủa

--- webBadminton/src/main/java/com/example/webBadminton/controller/BadmintonController.java
+++ webBadminton/src/main/java/com/example/webBadminton/controller/BadmintonController.java
@@ -12,11 +12,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
-@RequestMapping("/badmintons")
+@RequestMapping("/courts")
 public class BadmintonController {
     @GetMapping
     public String home() {
-        return "badminton/list";
+        return "court/list";
     }
 
 //    @Autowired
