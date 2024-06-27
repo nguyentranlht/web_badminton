b7d55881ed2e3312ae7ee89eccdeeed10fbaa743
nguyentranlht <88877923+nguyentranlht@users.noreply.github.com>
2024-06-27 20:48:55 +0700

    Detail badminton

--- webBadminton/src/main/java/com/example/webBadminton/repository/IBookingRepository.java
+++ webBadminton/src/main/java/com/example/webBadminton/repository/IBookingRepository.java
@@ -10,7 +10,7 @@ import java.time.LocalDate;
 import java.util.List;
 @Repository
 public interface IBookingRepository extends JpaRepository<BookingCourt, Long> {
-    @Query("SELECT b FROM BookingCourt b WHERE b.court.badmintonId = ?1 AND b.court.courtId = ?2 AND b.bookingDate = ?3")
+    @Query("SELECT b FROM BookingCourt b WHERE b.court.badmintonId = ?1 AND b.court.courtId = ?2 AND b.bookingDate = ?3 AND b.status = 'true'")
     List<BookingCourt> findBookingsByCourtAndDate(Long badmintonId, Long courtId, LocalDate date);
 
     List<BookingCourt> findByBookingDate(LocalDate date);
